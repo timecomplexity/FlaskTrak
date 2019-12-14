7afc52f8424dc60e46fd9b848c51189f2b85b0c5
blastbeatsandcode <alexsilcott@gmail.com>
2019-12-14 14:00:28 -0500

    Does some error handling and have a basic templating framework to work with.

--- server.py
+++ server.py
@@ -6,5 +6,13 @@ app = Flask(__name__, static_url_path='/static', template_folder='template')
 
 # Application routes determine the functions to call when the user tries to access a particular URL
 @app.route('/')
-def server_init():
+def index():
 	return render_template("index.html", title='FlaskTrak - Track your stuff!')
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+	return render_template('500.html'), 500
